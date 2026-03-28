57c79c324ff332049bc7be2ec8315c3835b6d8be
Emii-lia <fiaromiangaly@gmail.com>
2026-03-28 12:49:15 +0300

    refactor(sddm): switching to custom genshin sddm theme

--- sddm/setup.sh
+++ sddm/setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh | bash
+git clone https://github.com/Emii-lia/genshin-sddm-theme-re.git
+cd genshin-sddm-theme-re
+sudo ./install-sddm-theme.sh
